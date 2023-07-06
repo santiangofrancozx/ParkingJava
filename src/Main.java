4f0050b9a28730388dbe2b78dd9764de6102713a
VictorGH0 <victorlg346@gmail.com>
2023-07-05 23:26:16 -0500

    Cambios en el size del Frame

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,23 @@
-
+import Modelo.Delete_DB;
+import Modelo.Crear_BD;
 import Vista.*;
 import Controlador.*;
 import Vista.Users.Crear_Usuario;
-//import Modelo.*;
+import Vista.DataBase.*;
+import Modelo.*;
 
 
 public class Main{
     public static void main(String[] args) {
+        DataBase modelo = new DataBase();
         PrincipalMenu menu = new PrincipalMenu();
         Crear_Usuario cu = new Crear_Usuario(null, true);
+        Crear_DataBase crearDB = new Crear_DataBase(null, true);
+        Delete_DB dDB = new Delete_DB();
+        Crear_BD createDB = new Crear_BD();
         ControladorInicio cIni = new ControladorInicio(menu);
-        ControladorMenu cMenu = new ControladorMenu(menu, cu);
+        ControladorMenu cMenu = new ControladorMenu(menu, cu, crearDB, dDB);
+        ControladorDB cDB = new ControladorDB(modelo, crearDB, createDB);
         cIni.inicio();
     }
 }
