e680260384ee9af039118b8a160780d157fbedae
VictorGH0 <victorlg346@gmail.com>
2023-07-05 23:25:33 -0500

    Cambios en Controlador Menu

--- src/Controlador/ControladorMenu.java
+++ src/Controlador/ControladorMenu.java
@@ -4,17 +4,26 @@
  */
 package Controlador;
 
+import Modelo.*;
 import Vista.*;
-import Vista.Users.Crear_Usuario;
+import Vista.Users.*;
+import Vista.DataBase.*;
 import java.awt.event.*;
 
 public class ControladorMenu implements ActionListener{
     PrincipalMenu menu;
     Crear_Usuario cu;
-    public ControladorMenu(PrincipalMenu menu, Crear_Usuario cu){
+    Crear_DataBase crearDB;
+    Delete_DB dDB;
+    
+    public ControladorMenu(PrincipalMenu menu, Crear_Usuario cu, Crear_DataBase crearDB, Delete_DB dDB){
         this.menu = menu;
         this.cu = cu;
+        this.crearDB = crearDB;
+        this.dDB = dDB;
         this.menu.crearU.addActionListener(this);
+        this.menu.crearBD.addActionListener(this);
+        this.menu.eliminarBD.addActionListener(this);
     }
     
     @Override
@@ -22,13 +31,28 @@ public class ControladorMenu implements ActionListener{
         if(e.getSource() == menu.crearU){
             inicioCU();
         }
+        if(e.getSource() == menu.crearBD){
+            inicioCrearDB();
+        }
+        if(e.getSource() == menu.eliminarBD){
+            dDB.delete();
+        }
     }
     
     public void inicioCU(){
         cu.setTitle("Crear Usuario");
         cu.setSize(300, 300);
         cu.setLocationRelativeTo(null);
-        cu.setVisible(true);;
+        cu.setVisible(true);
     }
+    
+    public void inicioCrearDB(){
+        crearDB.setTitle("                      Creacion Base de Datos");
+        crearDB.setSize(330,270);
+        crearDB.setLayout(null);
+        crearDB.setLocationRelativeTo(null);
+        crearDB.setVisible(true);   
+    }
+   
             
 }
